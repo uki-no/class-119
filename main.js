3243c2d35f8f898072d1a0465abed38014da893b
uki-no <84130177+uki-no@users.noreply.github.com>
2023-06-05 16:54:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function setup(){
+    canvas= createCanvas(300,300)
+    canvas.center()
+    background("white")
+    canvas.mouseReleased(classifyCanvas)
+    synth= window.speechSynthesis
+}
+
+function preload(){
+    classifier= ml5.imageClassifier('DoodleNet')
+}
+function draw(){
+    strokeWeight(13)
+    stroke('black')
+    if (mouseIsPressed) {
+    line(pmouseX,pmouseY,mouseX,mouseY);
+    }   
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult)
+}
+
+function gotResult(error,results){
+if (error) {
+    console.log(error)
+} else {
+    console.log(results)
+    document.getElementById("label").innerHTML= "label=   "+results[0].label
+    document.getElementById("confidence").innerHTML="confidence=   "+Math.round(results[0].confidence*100)+"%"
+    utterThis= new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
+}
+
+function reset(){
+    background("white")
+}
